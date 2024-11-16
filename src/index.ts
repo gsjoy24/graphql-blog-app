42571afff7cd31b61fe83c935368bea4af70af5f
gjsoy24 <gour.joy24@gmail.com>
2024-11-17 01:56:23 +0600

    feat: enhance context typing for Apollo Server with PrismaClient integration

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
-import { PrismaClient } from "@prisma/client";
+import { Prisma, PrismaClient } from "@prisma/client";
+import { DefaultArgs } from "@prisma/client/runtime/library";
 import { resolvers } from "./resolvers";
 import { typeDefs } from "./schema";
 
 const prisma = new PrismaClient();
+
+interface Context {
+	prisma: PrismaClient<Prisma.PrismaClientOptions, never, DefaultArgs>;
+}
 
 const main = async () => {
 	const server = new ApolloServer({
@@ -13,7 +18,7 @@ const main = async () => {
 	});
 	const { url } = await startStandaloneServer(server, {
 		listen: { port: 4000 },
-		context: async () => ({ prisma })
+		context: async (): Promise<Context> => ({ prisma })
 	});
 
 	console.log(`🚀  Server ready at: ${url}`);
